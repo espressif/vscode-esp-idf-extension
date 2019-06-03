7caa59660f923d85539e7fdc42dcb303e55e5f52
Brian Alberto Ignacio Reyes <brian@espressif.com>
2019-06-03 17:01:08 +0800

    Add license field in package json

--- get_serial_list.py
+++ get_serial_list.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Espressif Systems (Shanghai) CO LTD
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """ Lists serial port names
 
     :raises ImportError:
@@ -9,6 +23,6 @@ import sys
 try:
     import serial.tools.list_ports
 except ImportError:
-    print 'Import has failed. Make sure pyserial is installed.'
+    print('Import has failed. Make sure pyserial is installed.')
     sys.exit(1)
 print([comport.device for comport in serial.tools.list_ports.comports()])
--- menuconfig/js/HtmlFormatter.js
+++ menuconfig/js/HtmlFormatter.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Espressif Systems (Shanghai) CO LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 define([], () => function HtmlFormatter() {
   const middleTextBulletRegex = /(\n\n-)[\s\S]*(\n\n)/g;
   const linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
