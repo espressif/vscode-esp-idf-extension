f39437b4d9c20a749f8e41b85c8bdbb3b9d8606d
Sergei Silnov <sergei.silnov@espressif.com>
2019-06-06 15:03:07 +0200

    Fix python linting and update yapf line length

--- ci/copyright_header_checker.py
+++ ci/copyright_header_checker.py
@@ -5,13 +5,13 @@
 # Project: ESP-IDF VSCode Extension
 # File Created: Tuesday, 4th June 2019 11:00:17 pm
 # Copyright 2019 Espressif Systems (Shanghai) CO LTD
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,7 @@ EXCLUDE_DIRS = ['node_modules']
 DEFAULT_EXTENSIONS = ["js", "ts", "py"]
 
 COPYRIGHT_REGEX = re.compile(r"Copyright [0-9]{4} Espressif Systems \(Shanghai\) CO\.? LTD\.?",
-                             flags=(re.MULTILINE | re.IGNORECASE))
+                             flags=re.MULTILINE | re.IGNORECASE)
 
 
 def contain_copyright_message(file_path):
