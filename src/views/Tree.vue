2e0d7856d05e3b4b6f43fbe9fde8035f67c3dd75
Soumesh Banerjee <soumesh.banerjee@espressif.com>
2019-09-05 18:45:02 +0530

    :sparkles: Add Tree for stack trace printing

---
+++ src/views/Tree.vue
@@ -0,0 +1,49 @@
+<template>
+  <li>
+    <div class="link">
+      <span @click="toggle" v-if="tree.child">{{ isOpen ? '▼' : '▶' }}</span>
+      <span v-else>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+      {{ tree.name }}
+    </div>
+    <ul v-show="isOpen" v-if="tree.child">
+      <Tree v-bind:tree="tree.child"></Tree>
+    </ul>
+  </li>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+const Tree = Vue.extend({
+  name: "Tree",
+  props: {
+    tree: Object
+  },
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggle() {
+      if (this.tree.child) {
+        this.isOpen = !this.isOpen;
+      }
+    }
+  }
+});
+export default Tree;
+</script>
+
+<style lang="scss" scoped>
+li {
+  list-style: none;
+}
+ul {
+  padding-left: 0.5em;
+  line-height: 1.5em;
+}
+.link {
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
