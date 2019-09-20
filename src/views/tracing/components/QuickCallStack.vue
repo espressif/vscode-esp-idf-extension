5a85eda48b4c7d8e5058d3a347b2ac7f58f31f99
Soumesh Banerjee <soumesh.banerjee@espressif.com>
2019-09-20 15:18:30 +0530

    Add call trace for plot component

---
+++ src/views/tracing/components/QuickCallStack.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="notification">
+    <button class="delete" @click="dismiss"></button>
+    <p class>
+      {{info.ts}}s: {{info.type}} {{info.size}} bytes @
+      <code>{{info.addr}}</code> in
+      <code>{{info.task}}</code> task
+    </p>
+    <stack-trace v-bind:tree="createTreeFromAddressArray(info.callers)"></stack-trace>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+const QuickCallStack = Vue.extend({
+    name: "QuickCallStack",
+    props: {
+        info: Object
+    },
+    methods: {
+        dismiss() {
+            this.$emit("dismiss");
+        },
+        createTreeFromAddressArray(e) {
+            return this.$root.createTreeFromAddressArray(e);
+        }
+    }
+});
+export default QuickCallStack;
+</script>
