4b88c6b752f52789bf816a63d073020a5e2d8bde
Soumesh Banerjee <soumesh.banerjee@espressif.com>
2019-09-24 01:36:03 +0530

    Add reverse call stack function

--- src/views/tracing/components/CallStack.vue
+++ src/views/tracing/components/CallStack.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="call-stack-container">
-    <div class="columns">
+  <div>
+    <div class="field has-addons">
+      <p class="control">
+        <button class="button" @click="isCallStackReverse = !isCallStackReverse">
+          <span class="icon is-small">
+            <i class="fas fa-history"></i>
+          </span>
+          <span>Reverse Call Stack</span>
+        </button>
+      </p>
+    </div>
+    <div class="columns headers">
       <div class="column is-2">Bytes Used</div>
       <div class="column is-2">Count</div>
       <div class="column">Function Name</div>
     </div>
-    <div v-for="(calls, index) in callstack" :key="index">
-      <calls v-bind:tree="createTreeFromAddressArray(calls)" :index="index" :space="1"></calls>
+    <div class="call-stack-container">
+      <div v-for="(calls, index) in callStack" :key="index">
+        <calls v-bind:tree="createTreeFromAddressArray(calls)" :index="index" :space="1"></calls>
+      </div>
     </div>
   </div>
 </template>
@@ -18,10 +30,29 @@ const CallStack = Vue.extend({
   props: {
     callstack: Array
   },
+  data() {
+    return {
+      isCallStackReverse: false,
+      reversed: false
+    };
+  },
   methods: {
     createTreeFromAddressArray(addr: string[]): any {
       const root = this.$root as any;
       return root.createTreeFromAddressArray(addr);
+    }
+  },
+  computed: {
+    callStack() {
+      if (this.isCallStackReverse) {
+        const tempCallStack = [];
+        this.callstack.forEach((calls, index) => {
+          tempCallStack.push(calls.slice().reverse());
+        });
+        return tempCallStack;
+      } else {
+        return this.callstack;
+      }
     }
   }
 });
@@ -29,14 +60,20 @@ export default CallStack;
 </script>
 
 <style lang="scss" scoped>
-.call-stack-container{
+.call-stack-container {
   overflow: auto;
   height: calc(45vh);
 }
-.columns{
+.columns {
   margin: 0 auto;
   border-bottom-width: 2px;
   border-bottom-style: solid;
   border-bottom-color: var(--vscode-foreground);
+}
+.column{
+  padding: 0.3rem;
+}
+button{
+  font-size: 10px;
 }
 </style>
