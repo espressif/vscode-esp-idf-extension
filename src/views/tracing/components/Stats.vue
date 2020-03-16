f81e029f751b125d6a034d4e10eaa8a2153f116a
Soumesh Banerjee <soumeshbanerjee@live.com>
2020-03-16 18:16:00 +0530

    Add stats table for HeapTracing

---
+++ src/views/tracing/components/Stats.vue
@@ -0,0 +1,47 @@
+<template>
+  <table
+    class="table is-fullwidth is-hoverable is-size-7-mobile is-size-6-tablet is-size-5-desktop"
+  >
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Persistent Count</td>
+        <td>{{ persistentCount }}</td>
+      </tr>
+      <tr>
+        <td>Persistent Bytes</td>
+        <td>{{ persistentBytes }}</td>
+      </tr>
+      <tr>
+        <td>Transient Count</td>
+        <td>{{ transientCount }}</td>
+      </tr>
+      <tr>
+        <td>Total Bytes</td>
+        <td>{{ totalBytes }}</td>
+      </tr>
+      <tr>
+        <td>Total Count</td>
+        <td>{{ totalCount }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    persistentCount: String,
+    persistentBytes: Number,
+    transientCount: Number,
+    totalBytes: Number,
+    totalCount: Number
+  }
+});
+</script>
