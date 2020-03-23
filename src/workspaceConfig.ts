cb8208a181ea85cc54270086fab690f8e6dd27bf
Soumesh Banerjee <soumeshbanerjee@live.com>
2020-03-23 23:45:55 +0530

    :fix: Fix merge error

--- src/workspaceConfig.ts
+++ src/workspaceConfig.ts
@@ -47,27 +47,32 @@ export function updateIdfComponentsTree(projectDescriptionPath: string) {
 }
 
 export function getProjectName(workspacePath: string): Promise<string> {
-    const projDescJsonPath = path.join(workspacePath, "build", "project_description.json");
-    return new Promise((resolve, reject) => {
-        try {
-            if (!utils.fileExists(projDescJsonPath)) {
-                return reject(new Error(`${projDescJsonPath} doesn't exist.`));
-            }
-            fs.readFile(projDescJsonPath, (err, data) => {
-                if (err) {
-                    Logger.error(err.message, err);
-                    return reject(err);
-                }
-                const projDescJson = JSON.parse(data.toString());
-                if (Object.prototype.hasOwnProperty.call(projDescJson, "project_name")) {
-                    return resolve(projDescJson.project_name);
-                } else {
-                    return reject(new Error(`project_name field doesn't exist in ${projDescJsonPath}.`));
-                }
-            });
-        } catch (error) {
-            Logger.error(error.message, error);
-            return reject(error);
+  const projDescJsonPath = path.join(
+    workspacePath,
+    "build",
+    "project_description.json"
+  );
+  return new Promise((resolve, reject) => {
+    try {
+      if (!utils.fileExists(projDescJsonPath)) {
+        return reject(new Error(`${projDescJsonPath} doesn't exist.`));
+      }
+      fs.readFile(projDescJsonPath, (err, data) => {
+        if (err) {
+          Logger.error(err.message, err);
+          return reject(err);
+        }
+        const projDescJson = JSON.parse(data.toString());
+        if (
+          Object.prototype.hasOwnProperty.call(projDescJson, "project_name")
+        ) {
+          return resolve(projDescJson.project_name);
+        } else {
+          return reject(
+            new Error(
+              `project_name field doesn't exist in ${projDescJsonPath}.`
+            )
+          );
         }
       });
     } catch (error) {
