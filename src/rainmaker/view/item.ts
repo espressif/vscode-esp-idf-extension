1d8029262d349191bbbba32875296033572c8c17
brian@espressif.com <brian@espressif.com>
2020-12-22 15:33:19 +0800

    rm unused type

--- src/rainmaker/view/item.ts
+++ src/rainmaker/view/item.ts
@@ -15,12 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {
-  TreeItem,
-  TreeItemLabel,
-  TreeItemCollapsibleState,
-  ThemeIcon,
-} from "vscode";
+import { TreeItem, TreeItemCollapsibleState, ThemeIcon } from "vscode";
 
 export enum RMakerItemType {
   None,
