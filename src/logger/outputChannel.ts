743c0d189a616c3e4213e55b55c32cefb293aafc
Radu <radu.rentea@gmail.com>
2022-05-31 14:09:32 +0300

    Edit tags format

--- src/logger/outputChannel.ts
+++ src/logger/outputChannel.ts
@@ -25,9 +25,9 @@ export class OutputChannel {
   public static appendLine(message: string, name?: string) {
     OutputChannel.checkInitialized();
     if(name) {
-      OutputChannel.instance.appendLine(`[${name}'s output started]`);
+      OutputChannel.instance.appendLine(`**${name}'s output started**`);
       OutputChannel.instance.appendLine(message);
-      OutputChannel.instance.appendLine(`[${name}'s output ended]`);
+      OutputChannel.instance.appendLine(`**${name}]'s output ended**`);
     } else {
       OutputChannel.instance.appendLine(message);
     }
@@ -36,9 +36,9 @@ export class OutputChannel {
   public static append(message: string, name?: string) {
     OutputChannel.checkInitialized();
     if(name) {
-      OutputChannel.instance.append(`[${name}]'s output started`);
+      OutputChannel.instance.appendLine(`**${name}'s output started**`);
       OutputChannel.instance.append(message);
-      OutputChannel.instance.append(`[${name}]'s output ended`);
+      OutputChannel.instance.appendLine(`**${name}]'s output ended**`);
     } else {
       OutputChannel.instance.append(message);
     }
