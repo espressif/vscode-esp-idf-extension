1e24e022d205005c058bb910aad48456b6186a6c
Brian A. Ignacio <brian.ignacio@espressif.com>
2023-04-28 15:36:21 +0800

    trim nvs csv columns (#945)
    * trim nvs csv columns
    
    * fix nvs partition csv
    
    * fix ci test

--- src/test/suite/nvsPartitionTable.test.ts
+++ src/test/suite/nvsPartitionTable.test.ts
@@ -8,11 +8,6 @@ import {
   minValues,
   maxValues
 } from "../../views/nvs-partition-table/util";
-// You can import and use all API from the "vscode" module
-// as well as import your extension to test it
-import * as vscode from "vscode";
-import { stringify } from "querystring";
-import { NvsPartitionTable } from "../../views/nvs-partition-table/store/index";
 import * as fse from "fs-extra";
 import { resolve } from "path";
 
