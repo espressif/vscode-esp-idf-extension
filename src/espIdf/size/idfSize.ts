5a1eff39b579088a165814f79dfd8999a01457f2
Radu <radu.rentea@gmail.com>
2023-09-21 17:07:12 +0300

    No duplicate functions

--- src/espIdf/size/idfSize.ts
+++ src/espIdf/size/idfSize.ts
@@ -68,7 +68,7 @@ export class IDFSize {
         this.workspaceRoot
       ) as string;
       const version = await utils.getEspIdfFromCMake(espIdfPath);
-      const formatArgs = utils.isVersionGreaterOrEqual("5.1.0", version)
+      const formatArgs = utils.compareVersion(version, "5.1.0") >= 0 
         ? ["--format", "json"]
         : ["--json"];
       const overview = await this.idfCommandInvoker([
