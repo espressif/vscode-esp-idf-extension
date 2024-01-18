f09b6385c6c99305113877f6bbc7b9e8dfba0816
Brian Ignacio <brian.ignacio@espressif.com>
2024-01-18 17:18:05 +0800

    rm save scope selection

--- src/views/setup/components/selectSaveScope.vue
+++ src/views/setup/components/selectSaveScope.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-import { useSetupStore } from '../store';
+import { useSetupStore } from "../store";
+import { computed } from "vue";
 const store = useSetupStore();
 
-const { saveScope } = storeToRefs(store);
+const configurationSettingLocation = computed(() => {
+  switch (store.saveScope) {
+    case 1:
+      return "User Settings.";
+    case 2:
+      return "Workspace Settings.";
+    case 3:
+      return "a Workspace Folder Settings.";
+  }
+});
 </script>
 
 <template>
   <div id="select-py-version">
     <div class="field">
       <div class="control centerize">
-        <label for="python-version-select" class="label"
-          >Select where to save these settings:</label
+        <label
+          for="python-version-select"
+          class="label"
+          title="Use the ESP-IDF: Select where to save configuration settings command to change where to save this setup settings."
         >
-        <div class="select">
-          <select v-model="saveScope" id="settings-location-select">
-            <option value="1">Global</option>
-            <option value="2">Workspace</option>
-            <option value="3">Workspace Folder</option>
-          </select>
-        </div>
+          Configuration settings will be saved in
+          {{ configurationSettingLocation }}</label
+        >
       </div>
     </div>
   </div>
