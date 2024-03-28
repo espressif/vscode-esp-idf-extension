35ea1753d9cbf9d3ed78d2b5ae06c2da45abfd71
Brian Ignacio <brian.ignacio@espressif.com>
2024-03-28 20:39:26 +0800

    initial idf version switcher

---
+++ src/versionSwitcher/index.ts
@@ -0,0 +1,76 @@
+/*
+ * Project: ESP-IDF VSCode Extension
+ * File Created: Thursday, 28th March 2024 7:18:49 pm
+ * Copyright 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ConfigurationTarget, window, WorkspaceFolder } from "vscode";
+import { getPreviousIdfSetups } from "../setup/existingIdfSetups";
+import { checkIdfSetup, useIdfSetupSettings } from "../setup/setupValidation/espIdfSetup";
+import { readParameter } from "../idfConfiguration";
+import { getIdfMd5sum } from "../setup/espIdfJson";
+import { getEspIdfFromCMake } from "../utils";
+import { IdfSetup } from "../views/setup/types";
+
+export async function selectIdfSetup(workspaceFolder: WorkspaceFolder) {
+  const idfSetups = await getPreviousIdfSetups(true);
+  if (idfSetups.length === 0) {
+    await window.showInformationMessage("No ESP-IDF Setups found");
+    return;
+  }
+  const idfSetupOptions = idfSetups.map((idfSetup) => {
+    return {
+      label: `Version: ${idfSetup.version} IDF_PATH: ${idfSetup.idfPath} IDF_TOOLS_PATH: ${idfSetup.toolsPath}`,
+      target: idfSetup,
+    };
+  });
+  const selectedIdfSetupOption = await window.showQuickPick(idfSetupOptions, {
+    placeHolder: "Select a ESP-IDF version to use",
+  });
+  if (!selectedIdfSetupOption) {
+    return;
+  }
+  await useIdfSetupSettings(
+    selectedIdfSetupOption.target,
+    ConfigurationTarget.WorkspaceFolder
+  );
+}
+
+export async function getCurrentIdfSetup(workspaceFolder: WorkspaceFolder){
+  let idfPath = readParameter("idf.espIdfPath", workspaceFolder);
+  const toolsPath = readParameter(
+    "idf.toolsPath",
+    workspaceFolder
+  ) as string;
+  const gitPath = readParameter("idf.gitPath", workspaceFolder);
+  const pythonPath = readParameter(
+    "idf.pythonBinPath",
+    workspaceFolder
+  ) as string;
+
+  const idfSetupId = getIdfMd5sum(idfPath);
+  const idfVersion = await getEspIdfFromCMake(idfPath);
+  const currentIdfSetup: IdfSetup = {
+    id: idfSetupId,
+    idfPath,
+    gitPath,
+    toolsPath,
+    python: pythonPath,
+    version: idfVersion,
+    isValid: false,
+  };
+  currentIdfSetup.isValid = await checkIdfSetup(currentIdfSetup);
+  return currentIdfSetup;
+}
