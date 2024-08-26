1fab9da5bc9bb2d9ed80a6a50d512eefa5b453e6
Radu <radu.rentea@gmail.com>
2024-08-26 16:05:07 +0300

    Fix error handling for unsuccessful attempt
    Fix lint
    
    Fix duplication

--- src/espIdf/setTarget/setTargetInIdf.ts
+++ src/espIdf/setTarget/setTargetInIdf.ts
@@ -72,16 +72,22 @@ export async function setTargetInIDF(
 
   setTargetArgs.push("set-target", selectedTarget.target);
   const pythonBinPath = await getVirtualEnvPythonPath(workspaceFolder.uri);
-  const setTargetResult = await spawn(pythonBinPath, setTargetArgs, {
-    cwd: workspaceFolder.uri.fsPath,
-    env: modifiedEnv,
-  });
-  Logger.info(setTargetResult.toString());
-  const msg = vscode.l10n.t(
-    "Target {0} Set Successfully.",
-    selectedTarget.target.toLocaleUpperCase()
-  );
-  OutputChannel.appendLineAndShow(msg, "Set Target");
-  Logger.infoNotify(msg);
-  setCCppPropertiesJsonCompilerPath(workspaceFolder.uri);
+  try {
+    const setTargetResult = await spawn(pythonBinPath, setTargetArgs, {
+      cwd: workspaceFolder.uri.fsPath,
+      env: modifiedEnv,
+    });
+    Logger.info(setTargetResult.toString());
+    const msg = vscode.l10n.t(
+      "Target {0} Set Successfully.",
+      selectedTarget.target.toLocaleUpperCase()
+    );
+    OutputChannel.appendLineAndShow(msg, "Set Target");
+    Logger.infoNotify(msg);
+    setCCppPropertiesJsonCompilerPath(workspaceFolder.uri);
+  } catch (error) {
+    throw new Error(
+      `Failed to set target ${selectedTarget.target}: ${error.message}.`
+    );
+  }
 }
