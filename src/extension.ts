9d74c7fc2e08fd112c78c0b29fa9307a24de66a0
Brian Ignacio <brian.ignacio@espressif.com>
2024-12-05 11:08:08 +0800

    fix new projext examples esp idf loading

--- src/extension.ts
+++ src/extension.ts
@@ -2145,16 +2145,21 @@ export async function activate(context: vscode.ExtensionContext) {
       notificationMode === idfConf.NotificationMode.Notifications
         ? vscode.ProgressLocation.Notification
         : vscode.ProgressLocation.Window;
-    let idfSetups = await getIdfSetups(true);
+    let idfSetups = await getIdfSetups(true, false);
     const currentIdfSetup = await getCurrentIdfSetup(workspaceRoot);
-    let setupsToUse = [...idfSetups, currentIdfSetup];
-    setupsToUse = setupsToUse.filter(
-      (setup, index, self) =>
-        index ===
-        self.findIndex(
-          (s) => s.idfPath === setup.idfPath && s.toolsPath === setup.toolsPath
-        )
-    );
+    const onlyValidIdfSetups = idfSetups.filter((i) => i.isValid);
+    const isCurrentSetupInList = onlyValidIdfSetups.findIndex((idfSetup) => {
+      return (
+        idfSetup.idfPath === currentIdfSetup.idfPath &&
+        idfSetup.toolsPath === currentIdfSetup.toolsPath
+      );
+    });
+    if (currentIdfSetup.isValid && isCurrentSetupInList === -1) {
+      onlyValidIdfSetups.push(currentIdfSetup);
+    }
+    if (onlyValidIdfSetups.length === 0) {
+      return;
+    }
     vscode.window.withProgress(
       {
         cancellable: false,
@@ -2170,7 +2175,7 @@ export async function activate(context: vscode.ExtensionContext) {
             context.extensionPath,
             progress,
             workspaceRoot,
-            setupsToUse
+            onlyValidIdfSetups
           );
           if (newProjectArgs) {
             NewProjectPanel.createOrShow(context.extensionPath, newProjectArgs);
