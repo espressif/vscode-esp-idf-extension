e344409d71842e532d4e4ef8628e8d6db9c54383
Brian Ignacio <brian.ignacio@espressif.com>
2024-12-11 17:59:58 +0800

    add eim verify idf setup functions

--- src/extension.ts
+++ src/extension.ts
@@ -172,6 +172,7 @@ import {
 } from "./cdtDebugAdapter/hexViewProvider";
 import { configureClangSettings } from "./clang";
 import { getIdfSetups } from "./eim/getExistingSetups";
+import { getEnvVariables } from "./eim/verifySetup";
 
 // Global variables shared by commands
 let workspaceRoot: vscode.Uri;
@@ -2075,7 +2076,11 @@ export async function activate(context: vscode.ExtensionContext) {
     });
   });
 
-  registerIDFCommand("espIdf.installManager", () => {
+  registerIDFCommand("espIdf.installManager", async () => {
+    const output = await getEnvVariables(
+      "/Users/brainignacio/.espressif/activate_idf_v5.3.2.sh",
+      true
+    );
     vscode.env.openExternal(vscode.Uri.parse(ESP.URL.InstallManager.Releases));
   });
 
