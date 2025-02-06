58a548941e11e32ec1e31b7623da57d2aa54a26d
Brian Ignacio <brian.ignacio@espressif.com>
2025-02-06 17:57:12 +0800

    rm old setup

--- src/cmdTreeView/cmdStore.ts
+++ src/cmdTreeView/cmdStore.ts
@@ -54,7 +54,6 @@ export enum CommandKeys {
 
 export enum AdvancedCommandKeys {
   InstallManager = "espIdf.installManager",
-  Setup = "espIdf.setup.start",
   Examples = "espIdf.examples.start",
   NewProject = "espIdf.newProject.start",
   Size = "espIdf.size",
@@ -85,11 +84,6 @@ export function createAdvancedCommandDictionary(): Record<
       checkboxState: undefined,
       iconId: "link-external",
       tooltip: l10n.t("Open ESP-IDF Installation Manager"),
-    },
-    [AdvancedCommandKeys.Setup]: {
-      checkboxState: undefined,
-      iconId: "extensions",
-      tooltip: l10n.t("Configure ESP-IDF Extension"),
     },
     [AdvancedCommandKeys.Examples]: {
       checkboxState: undefined,
