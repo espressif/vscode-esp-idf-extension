373c7f45fdf75c8ed7e1003bd92bec4057a09abf
Brian Ignacio <brian.ignacio@espressif.com>
2025-02-08 19:38:57 +0800

    update menuconfig test rm doc cmd

--- testFiles/testWorkspace/main/hello_world_main.c
+++ testFiles/testWorkspace/main/hello_world_main.c
@@ -5,14 +5,8 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
 void app_main(void)
 {
-    int i = 0;
-    while(1) {
-        printf("Hello world!\n", i);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+
 }
