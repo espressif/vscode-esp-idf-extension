602bf89bbe90b77558e5cb4d43b8f9fde0de61c9
Brian Ignacio <brian.ignacio@espressif.com>
2025-02-11 18:38:50 +0800

    fallback on global state setups

--- src/eim/loadSettings.ts
+++ src/eim/loadSettings.ts
@@ -2,13 +2,13 @@
  * Project: ESP-IDF VSCode Extension
  * File Created: Thursday, 6th February 2025 11:42:36 am
  * Copyright 2025 Espressif Systems (Shanghai) CO LTD
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,12 +23,20 @@ import { IdfSetup } from "./types";
 import { getEnvVarsFromIdfTools } from "../pythonManager";
 import { IdfToolsManager } from "../idfToolsManager";
 import { delimiter, dirname, join } from "path";
-import { getIdfPythonEnvPath } from "./migrationTool";
+import { getPythonEnvPath } from "./migrationTool";
 import { Logger } from "../logger/logger";
+import { readParameter } from "../idfConfiguration";
+import { pathExists } from "fs-extra";
 
-export async function getEnvVariables(idfSetup: IdfSetup, logToChannel = false) {
+export async function getEnvVariables(
+  idfSetup: IdfSetup,
+  logToChannel = false
+) {
   if (idfSetup.activationScript) {
-    return await getEnvVariablesFromActivationScript(idfSetup.activationScript, logToChannel);
+    return await getEnvVariablesFromActivationScript(
+      idfSetup.activationScript,
+      logToChannel
+    );
   } else {
     return await getEnvVariablesFromIdfSetup(idfSetup);
   }
@@ -105,16 +113,6 @@ export async function getEnvVariablesFromIdfSetup(idfSetup: IdfSetup) {
   let envVars: { [key: string]: string } = {};
   envVars["IDF_PATH"] = idfSetup.idfPath;
   envVars["IDF_TOOLS_PATH"] = idfSetup.toolsPath;
-  if (idfSetup.python) {
-    envVars["IDF_PYTHON_ENV_PATH"] = dirname(dirname(idfSetup.python));
-  } else {
-    idfSetup.python = await getIdfPythonEnvPath(
-      idfSetup.idfPath,
-      idfSetup.toolsPath,
-      idfSetup.sysPythonPath
-    );
-    envVars["IDF_PYTHON_ENV_PATH"] = dirname(dirname(idfSetup.python));
-  }
   const idfToolsManager = await IdfToolsManager.createIdfToolsManager(
     idfSetup.idfPath
   );
@@ -124,16 +122,33 @@ export async function getEnvVariablesFromIdfSetup(idfSetup: IdfSetup) {
   );
   envVars["PATH"] = exportedToolsPaths;
   const idfToolsVars = await idfToolsManager.exportVars(idfSetup.toolsPath);
-  const idfVars = await getEnvVarsFromIdfTools(
-    idfSetup.idfPath,
-    idfSetup.toolsPath,
-    idfSetup.python
-  );
+
   for (const toolVar in idfToolsVars) {
     envVars[toolVar] = idfToolsVars[toolVar];
   }
-  for (const idfVar in idfVars) {
-    envVars[idfVar] = idfVars[idfVar];
+
+  if (!idfSetup.python) {
+    if (!idfSetup.sysPythonPath) {
+      idfSetup.sysPythonPath = readParameter("idf.pythonInstallPath") as string;
+    }
+    idfSetup.python = await getPythonEnvPath(
+      idfSetup.idfPath,
+      idfSetup.toolsPath,
+      idfSetup.sysPythonPath
+    );
+  }
+  const pythonExists = await pathExists(idfSetup.python);
+
+  if (pythonExists) {
+    envVars["IDF_PYTHON_ENV_PATH"] = dirname(dirname(idfSetup.python));
+    const idfVars = await getEnvVarsFromIdfTools(
+      idfSetup.idfPath,
+      idfSetup.toolsPath,
+      idfSetup.python
+    );
+    for (const idfVar in idfVars) {
+      envVars[idfVar] = idfVars[idfVar];
+    }
   }
   return envVars;
 }
