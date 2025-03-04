82d51b1ad44c338c92d1293686f8b296bab3910b
Brian Ignacio <brian.ignacio@espressif.com>
2025-03-04 17:01:29 +0800

    read esp idf setups from esp_idf.json

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,6 @@ export namespace ESP {
 
   export namespace GlobalConfiguration {
     export let store: ExtensionConfigStore;
-    export const IDF_SETUPS = "IDF_SETUPS";
     export const SELECTED_WORKSPACE_FOLDER = "SELECTED_WORKSPACE_FOLDER";
   }
 
