047b9e16a80fa7339af70223f63a69b7a33ee054
Brian A. Ignacio <brian.ignacio@espressif.com>
2025-05-21 15:52:33 +0800

    execute export script allow use idf.customTerminalExecutable in IDF Terminal (#1558)
    * execute export script allow use idf.customTerminalExecutable in esp idf terminal
    
    * update validation for shellExecutablePath
    
    * fix workspace used
    
    * Fix getTargetsFromEspIdf when givenIdfPathDir defined
    
    * rm call idf py to get supported or preview target parse constants py

--- src/extension.ts
+++ src/extension.ts
@@ -4418,23 +4418,39 @@ function createIdfTerminal(extensionPath: string) {
         shellArgs = ["-ExecutionPolicy", "Bypass"];
       }
     }
+    let shellExecutablePath = idfConf.readParameter(
+      "idf.customTerminalExecutable",
+      workspaceRoot
+    ) as string;
+    const shellExecutableArgs = idfConf.readParameter(
+      "idf.customTerminalExecutableArgs",
+      workspaceRoot
+    ) as string[];
+    if (!shellExecutablePath) {
+      shellExecutablePath = vscode.env.shell;
+    }
+    if (shellExecutableArgs && shellExecutableArgs.length) {
+      shellArgs = shellExecutableArgs;
+    }
     const espIdfTerminal = vscode.window.createTerminal({
       name: "ESP-IDF Terminal",
       env: modifiedEnv,
       cwd: workspaceRoot.fsPath || modifiedEnv.IDF_PATH || process.cwd(),
       strictEnv: true,
       shellArgs,
-      shellPath: vscode.env.shell,
+      shellPath: shellExecutablePath,
     });
     if (process.platform === "win32") {
-      if (vscode.env.shell.indexOf("cmd.exe") !== -1) {
-        espIdfTerminal.sendText(`"${path.join(extensionPath, "export.bat")}"`);
+      if (shellExecutablePath.indexOf("cmd.exe") !== -1) {
+        espIdfTerminal.sendText(
+          `call "${path.join(extensionPath, "export.bat")}"`
+        );
       } else if (
-        vscode.env.shell.indexOf("powershell") !== -1 ||
-        vscode.env.shell.indexOf("pwsh") !== -1
+        shellExecutablePath.indexOf("powershell") !== -1 ||
+        shellExecutablePath.indexOf("pwsh") !== -1
       ) {
         espIdfTerminal.sendText(
-          `'${path.join(extensionPath, "export.ps1").replace(/'/g, "''")}'`
+          `& '${path.join(extensionPath, "export.ps1").replace(/'/g, "''")}'`
         );
       }
     }
