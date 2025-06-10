70e5b27fcafa62497494714513384d4f5a9cf19e
Brian Ignacio <brian.ignacio@espressif.com>
2025-06-10 09:03:08 +0800

    rm use of ESP_IDF_VERSION in status bar

--- src/extension.ts
+++ src/extension.ts
@@ -434,14 +434,15 @@ export async function activate(context: vscode.ExtensionContext) {
             const currentEnvVars = ESP.ProjectConfiguration.store.get<{
               [key: string]: string;
             }>(ESP.ProjectConfiguration.CURRENT_IDF_CONFIGURATION, {});
+            const idfVersion = await utils.getEspIdfFromCMake(
+              currentEnvVars["IDF_PATH"]
+            );
             if (statusBarItems["currentIdfVersion"]) {
-              statusBarItems["currentIdfVersion"].text = currentEnvVars[
-                "ESP_IDF_VERSION"
-              ]
+              statusBarItems["currentIdfVersion"].text = idfVersion
                 ? `$(${
                     commandDictionary[CommandKeys.SelectCurrentIdfVersion]
                       .iconId
-                  }) ESP-IDF ${currentEnvVars["ESP_IDF_VERSION"]}`
+                  }) ESP-IDF v${idfVersion}`
                 : `$(${
                     commandDictionary[CommandKeys.SelectCurrentIdfVersion]
                       .iconId
@@ -1039,13 +1040,15 @@ export async function activate(context: vscode.ExtensionContext) {
         const currentEnvVars = ESP.ProjectConfiguration.store.get<{
           [key: string]: string;
         }>(ESP.ProjectConfiguration.CURRENT_IDF_CONFIGURATION, {});
+
+        const idfVersion = await utils.getEspIdfFromCMake(
+          currentEnvVars["IDF_PATH"]
+        );
         if (statusBarItems["currentIdfVersion"]) {
-          statusBarItems["currentIdfVersion"].text = currentEnvVars[
-            "ESP_IDF_VERSION"
-          ]
+          statusBarItems["currentIdfVersion"].text = idfVersion
             ? `$(${
                 commandDictionary[CommandKeys.SelectCurrentIdfVersion].iconId
-              }) ESP-IDF ${currentEnvVars["ESP_IDF_VERSION"]}`
+              }) ESP-IDF v${idfVersion}`
             : `$(${
                 commandDictionary[CommandKeys.SelectCurrentIdfVersion].iconId
               }) ESP-IDF InvalidSetup`;
