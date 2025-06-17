4e6731c73b4279a0a6c120aea2df6cd6ddcb103f
Brian Ignacio <brian.ignacio@espressif.com>
2025-06-17 19:11:43 +0800

    partition table

--- src/views/partition-table/components/PartitionTable.vue
+++ src/views/partition-table/components/PartitionTable.vue
@@ -1,49 +1,85 @@
-
 <script setup lang="ts"></script>
 <template>
-  <div class="section no-padding-top">
-    <div class="container is-mobile">
-      <nav class="level is-mobile">
-        <div class="level-left"></div>
-        <div class="level-right">
-          <div class="field has-addons">
-            <p class="control">
-              <button
-                class="button is-size-7-mobile is-size-7-tablet"
-                @click="$emit('add')"
-              >
-                Add New Row
-              </button>
-            </p>
-            <p class="control">
-              <button
-                class="button is-size-7-mobile is-size-7-tablet"
-                @click="$emit('save')"
-              >
-                Save
-              </button>
-            </p>
-          </div>
-        </div>
-      </nav>
-      <div>
-        <table class="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Sub Type</th>
-              <th>Offset</th>
-              <th>Size</th>
-              <th>Encrypted</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <slot></slot>
-          </tbody>
-        </table>
+  <div class="settings-table">
+    <div class="settings-table-header">
+      <div class="settings-table-actions">
+        <button
+          class="vscode-button"
+          @click="$emit('add')"
+        >
+          Add New Row
+        </button>
+        <button
+          class="vscode-button"
+          @click="$emit('save')"
+        >
+          Save
+        </button>
       </div>
+    </div>
+    <div class="settings-table-content">
+      <table class="table is-fullwidth">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Sub Type</th>
+            <th>Offset</th>
+            <th>Size</th>
+            <th>Encrypted</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <slot></slot>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.settings-table {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.settings-table-header {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.settings-table-actions {
+  display: flex;
+  gap: 0.5rem;
+}
+
+.vscode-button {
+  height: 28px;
+  padding: 0 12px;
+  background-color: var(--vscode-button-background);
+  color: var(--vscode-button-foreground);
+  border: none;
+  border-radius: 2px;
+  font-size: 13px;
+  font-weight: 400;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.vscode-button:hover {
+  background-color: var(--vscode-button-hoverBackground);
+}
+
+.vscode-button:focus {
+  outline: 1px solid var(--vscode-focusBorder);
+  outline-offset: -1px;
+}
+
+.settings-table-content {
+  overflow-x: auto;
+}
+</style>
