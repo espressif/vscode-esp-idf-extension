0c35f1efbcf855b950810dd1d7ca7e454cdbb655
Brian Ignacio <brian.ignacio@espressif.com>
2025-06-26 17:10:27 +0800

    add production docs for release branches

---
+++ docs_espressif/_static/docs_version.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_VERSIONS = {
+    DEFAULTS: { has_targets: false,
+                supported_targets: [ "esp32" ]
+              },
+    VERSIONS: [
+        { name: "latest" },
+        { name: "release-v2.0", pre_release: true }
+    ]
+};
