a7d1b959e313380d6019c39a18107584772dcf9c
Brian A. Ignacio <brian.ignacio@espressif.com>
2025-07-04 12:36:16 +0800

    Add release branches docs. (VSC-1686) (#1599)
    * add production docs for release branches
    
    * update vsce ovsx
    
    * add pre release flow for rc tags
    
    * move ovsx prerelease to package json
    
    * add old false see if it will be moved to stable
    
    * master instead of latest

---
+++ docs_espressif/_static/docs_version.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_VERSIONS = {
+    DEFAULTS: { has_targets: false,
+                supported_targets: [ "esp32" ]
+              },
+    VERSIONS: [
+        { name: "master", old: false },
+        { name: "release-v2.0", pre_release: true }
+    ],
+};
