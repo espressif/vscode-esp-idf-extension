1124580e19e982ac2b1ed2619d28a883eb645e2a
Brian Ignacio <brian.ignacio@espressif.com>
2025-07-08 18:42:29 +0800

    update options as object

--- src/ui-test/.mocharc-ci.js
+++ src/ui-test/.mocharc-ci.js
@@ -1,5 +1,7 @@
 module.exports = {
     timeout: 99999999,
     reporter: 'json',
-    'reporter-option': ['output=./out/ui-test-results.json']
+    'reporter-option': {
+        output: './out/ui-test-results.json'
+    }
 } 
