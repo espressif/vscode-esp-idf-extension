0b1d796d360962c6db3296d0d8a5530a6a5bcee6
Brian Ignacio <brian.ignacio@espressif.com>
2025-07-08 19:14:42 +0800

    update reporter option

--- src/ui-test/.mocharc-ci.js
+++ src/ui-test/.mocharc-ci.js
@@ -1,7 +1,5 @@
 module.exports = {
-    timeout: 99999999,
-    reporter: 'json',
-    'reporter-option': {
-        output: './out/ui-test-results.json'
-    }
-} 
+  timeout: 99999999,
+  reporter: "json",
+  "reporter-option": ["output=./out/ui-test-results.json"]
+};
