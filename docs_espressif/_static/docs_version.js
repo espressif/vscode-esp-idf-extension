3d26b98de42b542b6f01f0285b75921cfb34d04a
Brian Ignacio <brian.ignacio@espressif.com>
2025-07-31 17:00:42 +0800

    use latest instead of master in docs

--- docs_espressif/_static/docs_version.js
+++ docs_espressif/_static/docs_version.js
@@ -3,7 +3,7 @@ var DOCUMENTATION_VERSIONS = {
                 supported_targets: [ "esp32" ]
               },
     VERSIONS: [
-        { name: "master", old: false },
+        { name: "latest", old: false },
         { name: "release-v2.0", pre_release: true }
     ],
 };
