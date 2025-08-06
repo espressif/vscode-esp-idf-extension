5efb5dc1b147909e5c8e9e71726801ab44a92e28
radurentea <radu.rentea@gmail.com>
2025-08-06 17:50:52 +0300

    Standardize naming in status bar items

--- src/qemu/qemuManager.ts
+++ src/qemu/qemuManager.ts
@@ -215,7 +215,7 @@ export class QemuManager extends EventEmitter {
     const pythonBinPath = await getVirtualEnvPythonPath(workspaceFolder);
     this.qemuTerminal.sendText(`${pythonBinPath} ${qemuArgs.join(" ")}`);
     this.qemuTerminal.show(true);
-    this.updateStatusText("❇️ ESP-IDF: QEMU Server (Running)");
+          this.updateStatusText("❇️ QEMU Server (Running)");
   }
 
   public stop() {
@@ -224,7 +224,7 @@ export class QemuManager extends EventEmitter {
       this.qemuTerminal.dispose();
       this.qemuTerminal = undefined;
     }
-    this.updateStatusText("❌ ESP-IDF: QEMU Server (Stopped)");
+          this.updateStatusText("❌ QEMU Server (Stopped)");
   }
 
   public showOutputChannel(preserveFocus?: boolean) {
@@ -240,7 +240,7 @@ export class QemuManager extends EventEmitter {
         StatusBarAlignment.Left,
         0
       );
-      this._statusBarItem.name = this._statusBarItem.text = "[ESP-IDF: QEMU]";
+      this._statusBarItem.name = this._statusBarItem.text = "QEMU";
       const commandDictionary = createCommandDictionary();
       this._statusBarItem.tooltip =
         commandDictionary[CommandKeys.QemuServer].tooltip;
