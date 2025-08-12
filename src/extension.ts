079638279295c92739931f971c28665bdafc3f89
Brian Ignacio <brian.ignacio@espressif.com>
2025-08-12 13:27:21 +0800

    fix lint

--- src/extension.ts
+++ src/extension.ts
@@ -1481,16 +1481,13 @@ export async function activate(context: vscode.ExtensionContext) {
       });
     }
   );
-  
-  registerIDFCommand(
-    "espIdf.openImageViewer",
-    () => {
-      return PreCheck.perform([openFolderCheck], () => {
-        // Show the ImageViewPanel without an image
-        ImageViewPanel.show(context.extensionPath);
-      });
-    }
-  );
+
+  registerIDFCommand("espIdf.openImageViewer", () => {
+    return PreCheck.perform([openFolderCheck], () => {
+      // Show the ImageViewPanel without an image
+      ImageViewPanel.show(context.extensionPath);
+    });
+  });
 
   registerIDFCommand("espIdf.genCoverage", () => {
     return PreCheck.perform([openFolderCheck], async () => {
