a83c36d69c5aec3b64f4d52ce620ae5bf59a7054
Brian Ignacio <brian.ignacio@espressif.com>
2025-10-22 12:50:41 +0800

    update openOCD fallback boards definition

--- src/espIdf/openOcd/defaultBoards.ts
+++ src/espIdf/openOcd/defaultBoards.ts
@@ -20,87 +20,303 @@ import { IdfBoard } from "./boardConfiguration";
 
 export const defaultBoards = [
   {
-    name: "ESP32 module",
-    description: "ESP32 used with ESP-PROG board",
+    name: "ESP-WROVER-KIT 3.3V",
+    description: "ESP-WROVER-KIT with 3.3V ESP32-WROVER-B module",
     target: "esp32",
-    configFiles: ["interface/ftdi/esp32_devkitj_v1.cfg", "target/esp32.cfg"],
+    configFiles: ["board/esp32-wrover-kit-3.3v.cfg"],
   } as IdfBoard,
   {
-    name: "ESP32-C3 chip (via ESP-PROG)",
-    description: "ESP32-C3 used with ESP-PROG board",
-    target: "esp32c3",
-    configFiles: ["board/esp32c3-ftdi.cfg"],
+    name: "ESP-WROVER-KIT 1.8V",
+    description: "ESP-WROVER-KIT with 1.8V ESP32-WROVER-B module",
+    target: "esp32",
+    configFiles: ["board/esp32-wrover-kit-1.8v.cfg"],
   } as IdfBoard,
   {
-    name: "ESP32-C3 chip (via ESP-PROG-2)",
-    description: "ESP32-C3 debugging via ESP-PROG-2 board",
-    target: "esp32c3",
-    configFiles: ["board/esp32c3-bridge.cfg"],
+    name: "ESP32-ETHERNET-KIT",
+    description: "ESP32-ETHERNET-KIT with ESP32-WROVER-E module",
+    target: "esp32",
+    configFiles: ["board/esp32-ethernet-kit-3.3v.cfg"],
   } as IdfBoard,
   {
-    name: "ESP32-C3 chip (via builtin USB-JTAG)",
-    description: "ESP32-C3 debugging via builtin USB-JTAG",
-    target: "esp32c3",
-    configFiles: ["board/esp32c3-builtin.cfg"],
+    name: "ESP32 chip (via ESP-PROG)",
+    description: "ESP32 debugging via ESP-PROG board",
+    target: "esp32",
+    configFiles: [
+      "interface/ftdi/esp_ftdi.cfg",
+      "target/esp32.cfg"
+    ],
   } as IdfBoard,
   {
-    name: "ESP32-C6 chip (via ESP-PROG)",
-    description: "ESP32-C6 used with ESP-PROG board",
-    target: "esp32c6",
-    configFiles: ["board/esp32c6-ftdi.cfg"],
+    name: "ESP32 chip (via ESP-PROG-2)",
+    description: "ESP32 debugging via ESP-PROG-2 board",
+    target: "esp32",
+    configFiles: [
+      "board/esp32-bridge.cfg"
+    ],
   } as IdfBoard,
   {
-    name: "ESP32-C6 chip (via ESP-PROG-2)",
-    description: "ESP32-C6 debugging via ESP-PROG-2 board",
-    target: "esp32c6",
-    configFiles: ["board/esp32c6-bridge.cfg"],
+    name: "ESP32-SOLO-1 module (via ESP-PROG)",
+    description: "ESP32-SOLO-1 debugging via ESP-PROG board",
+    target: "esp32",
+    configFiles: [
+      "interface/ftdi/esp_ftdi.cfg",
+      "target/esp32-solo-1.cfg"
+    ],
   } as IdfBoard,
   {
-    name: "ESP32-C6 chip (via builtin USB-JTAG)",
-    description: "ESP32-C6 debugging via builtin USB-JTAG",
-    target: "esp32c6",
-    configFiles: ["board/esp32c6-builtin.cfg"],
-  } as IdfBoard,
-  {
-    name: "ESP32-H2 chip (via builtin USB-JTAG)",
-    description: "ESP32-H2 debugging via builtin USB-JTAG",
-    target: "esp32h2",
-    configFiles: ["board/esp32h2-builtin.cfg"],
-  } as IdfBoard,
-  {
-    name: "ESP32-H2 chip (via ESP-PROG)",
-    description: "ESP32-H2 debugging via ESP-PROG board",
-    target: "esp32h2",
-    configFiles: [ "board/esp32h2-ftdi.cfg"],
-  } as IdfBoard,
-  {
-    name: "ESP32-H2 chip (via ESP-PROG-2)",
-    description: "ESP32-H2 debugging via ESP-PROG-2 board",
-    target: "esp32h2",
-    configFiles: ["board/esp32h2-bridge.cfg"],
-  } as IdfBoard,
-  {
-    name: "ESP32-S2 module",
-    description: "ESP32-S2 used with ESP-PROG board",
+    name: "ESP32-S2-KALUGA-1",
+    description: "ESP32-S2-KALUGA-1 kit",
     target: "esp32s2",
-    configFiles: ["interface/ftdi/esp32_devkitj_v1.cfg", "target/esp32s2.cfg"],
+    configFiles: ["board/esp32s2-kaluga-1.cfg"],
+  } as IdfBoard,
+  {
+    name: "ESP32-S2 chip (via ESP-PROG)",
+    description: "ESP32-S2 debugging via ESP-PROG board",
+    target: "esp32s2",
+    configFiles: [
+      "interface/ftdi/esp_ftdi.cfg",
+      "target/esp32s2.cfg"
+    ],
   } as IdfBoard,
   {
     name: "ESP32-S2 chip (via ESP-PROG-2)",
     description: "ESP32-S2 debugging via ESP-PROG-2 board",
     target: "esp32s2",
-    configFiles: ["board/esp32s2-bridge.cfg"],
-  } as IdfBoard,
-  {
-    name: "ESP32-S3 chip (via ESP-PROG)",
-    description: "ESP32-S3 used with ESP-PROG board",
-    target: "esp32s3",
-    configFiles: ["interface/ftdi/esp32_devkitj_v1.cfg", "target/esp32s3.cfg"],
+    configFiles: [
+      "board/esp32s2-bridge.cfg"
+    ],
   } as IdfBoard,
   {
     name: "ESP32-S3 chip (via builtin USB-JTAG)",
     description: "ESP32-S3 debugging via builtin USB-JTAG",
     target: "esp32s3",
-    configFiles: ["board/esp32s3-builtin.cfg"],
+    configFiles: [
+      "board/esp32s3-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-S3 chip (via ESP-PROG)",
+    description: "ESP32-S3 debugging via ESP-PROG board",
+    target: "esp32s3",
+    configFiles: [
+      "interface/ftdi/esp_ftdi.cfg",
+      "target/esp32s3.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-S3 chip (via ESP-PROG-2)",
+    description: "ESP32-S3 debugging via ESP-PROG-2 board",
+    target: "esp32s3",
+    configFiles: [
+      "board/esp32s3-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C2 chip (via ESP-PROG)",
+    description: "ESP32-C2 debugging via ESP-PROG board",
+    target: "esp32c2",
+    configFiles: [
+      "board/esp32c2-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C2 chip (via ESP-PROG-2)",
+    description: "ESP32-C2 debugging via ESP-PROG-2 board",
+    target: "esp32c2",
+    configFiles: [
+      "board/esp32c2-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C3 chip (via builtin USB-JTAG)",
+    description: "ESP32-C3 debugging via builtin USB-JTAG",
+    target: "esp32c3",
+    configFiles: [
+      "board/esp32c3-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C3 chip (via ESP-PROG)",
+    description: "ESP32-C3 debugging via ESP-PROG board",
+    target: "esp32c3",
+    configFiles: [
+      "board/esp32c3-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C3 chip (via ESP-PROG-2)",
+    description: "ESP32-C3 debugging via ESP-PROG-2 board",
+    target: "esp32c3",
+    configFiles: [
+      "board/esp32c3-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C5 chip (via builtin USB-JTAG)",
+    description: "ESP32-C5 debugging via builtin USB-JTAG",
+    target: "esp32c5",
+    configFiles: [
+      "board/esp32c5-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C5 chip (via ESP-PROG)",
+    description: "ESP32-C5 debugging via ESP-PROG board",
+    target: "esp32c5",
+    configFiles: [
+      "board/esp32c5-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C5 chip (via ESP-PROG-2)",
+    description: "ESP32-C5 debugging via ESP-PROG-2 board",
+    target: "esp32c5",
+    configFiles: [
+      "board/esp32c5-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C6 chip (via builtin USB-JTAG)",
+    description: "ESP32-C6 debugging via builtin USB-JTAG",
+    target: "esp32c6",
+    configFiles: [
+      "board/esp32c6-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C6 chip (via ESP-PROG)",
+    description: "ESP32-C6 debugging via ESP-PROG board",
+    target: "esp32c6",
+    configFiles: [
+      "board/esp32c6-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C6 chip (via ESP-PROG-2)",
+    description: "ESP32-C6 debugging via ESP-PROG-2 board",
+    target: "esp32c6",
+    configFiles: [
+      "board/esp32c6-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C6 chip with LP core (via builtin USB-JTAG)",
+    description: "ESP32-C6 with LP core debugging via builtin USB-JTAG",
+    target: "esp32c6",
+    configFiles: [
+      "board/esp32c6-lpcore-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C6 chip with LP core (via ESP-PROG)",
+    description: "ESP32-C6 with LP core debugging via ESP-PROG board",
+    target: "esp32c6",
+    configFiles: [
+      "board/esp32c6-lpcore-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C6 chip with LP core (via ESP-PROG-2)",
+    description: "ESP32-C6 with LP core debugging via ESP-PROG-2 board",
+    target: "esp32c6",
+    configFiles: [
+      "board/esp32c6-lpcore-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C61 chip (via builtin USB-JTAG)",
+    description: "ESP32-C61 debugging via builtin USB-JTAG",
+    target: "esp32c61",
+    configFiles: [
+      "board/esp32c61-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C61 chip (via ESP-PROG)",
+    description: "ESP32-C61 debugging via ESP-PROG board",
+    target: "esp32c61",
+    configFiles: [
+      "board/esp32c61-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-C61 chip (via ESP-PROG-2)",
+    description: "ESP32-C61 debugging via ESP-PROG-2 board",
+    target: "esp32c61",
+    configFiles: [
+      "board/esp32c61-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-H2 chip (via builtin USB-JTAG)",
+    description: "ESP32-H2 debugging via builtin USB-JTAG",
+    target: "esp32h2",
+    configFiles: [
+      "board/esp32h2-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-H2 chip (via ESP-PROG)",
+    description: "ESP32-H2 debugging via ESP-PROG board",
+    target: "esp32h2",
+    configFiles: [
+      "board/esp32h2-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-H2 chip (via ESP-PROG-2)",
+    description: "ESP32-H2 debugging via ESP-PROG-2 board",
+    target: "esp32h2",
+    configFiles: [
+      "board/esp32h2-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-H4 chip (via builtin USB-JTAG)",
+    description: "ESP32-H4 debugging via builtin USB-JTAG",
+    target: "esp32h4",
+    configFiles: [
+      "board/esp32h4-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-H4 chip (via ESP-PROG)",
+    description: "ESP32-H4 debugging via ESP-PROG board",
+    target: "esp32h4",
+    configFiles: [
+      "board/esp32h4-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-H4 chip (via ESP-PROG-2)",
+    description: "ESP32-H4 debugging via ESP-PROG-2 board",
+    target: "esp32h4",
+    configFiles: [
+      "board/esp32h4-bridge.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-P4 chip (via builtin USB-JTAG)",
+    description: "ESP32-P4 debugging via builtin USB-JTAG",
+    target: "esp32p4",
+    configFiles: [
+      "board/esp32p4-builtin.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-P4 chip (via ESP-PROG)",
+    description: "ESP32-P4 debugging via ESP-PROG board",
+    target: "esp32p4",
+    configFiles: [
+      "board/esp32p4-ftdi.cfg"
+    ],
+  } as IdfBoard,
+  {
+    name: "ESP32-P4 chip (via ESP-PROG-2)",
+    description: "ESP32-P4 debugging via ESP-PROG-2 board",
+    target: "esp32p4",
+    configFiles: [
+      "board/esp32p4-bridge.cfg"
+    ],
   } as IdfBoard,
 ]; 
