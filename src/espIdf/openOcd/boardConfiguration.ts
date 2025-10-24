3408a49a8678008703869328c9dbb301381e8d03
Radu <radu.rentea@gmail.com>
2025-10-24 11:16:19 +0300

    Add: Translations

--- src/espIdf/openOcd/boardConfiguration.ts
+++ src/espIdf/openOcd/boardConfiguration.ts
@@ -266,7 +266,9 @@ export async function selectOpenOcdConfigFiles(
         const selectedBoard = boardQuickPick.selectedItems[0];
         if (!selectedBoard) {
           Logger.infoNotify(
-            `ESP-IDF board not selected. Remember to set the configuration files for OpenOCD with idf.openOcdConfigs`
+            l10n.t(
+              "ESP-IDF board not selected. Remember to set the configuration files for OpenOCD with idf.openOcdConfigs"
+            )
           );
         } else {
           const customExtraVarsRead = readParameter(
@@ -321,7 +323,9 @@ export async function selectOpenOcdConfigFiles(
           } else if (selectedBoard.target) {
             if (selectedBoard.label.indexOf("Custom board") !== -1) {
               const inputBoard = await window.showInputBox({
-                placeHolder: "Enter comma-separated configuration files",
+                placeHolder: l10n.t(
+                  "Enter comma-separated configuration files"
+                ),
                 value: selectedBoard.target.configFiles.join(","),
               });
               if (inputBoard) {
