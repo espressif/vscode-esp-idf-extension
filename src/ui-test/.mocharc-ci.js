e05eeee71a5dc17f2766c61c6eb28119fab2b649
AndriiFilippov <andrii.filippov@espressif.com>
2026-01-15 16:30:17 +0100

    fix: add new reporter

--- src/ui-test/.mocharc-ci.js
+++ src/ui-test/.mocharc-ci.js
@@ -1,24 +1,9 @@
-const fs = require("fs");
 const path = require("path");
-
-let outPath;
-
-const possiblePaths = [
-  path.resolve(__dirname, "../../out/ui-test-results.json"),
-  path.resolve(__dirname, "../out/ui-test-results.json"),
-  path.resolve(__dirname, "out/ui-test-results.json"),
-];
-
-for (const p of possiblePaths) {
-  // Use the first path where the folder exists
-  if (fs.existsSync(path.dirname(p))) {
-    outPath = p;
-    break;
-  }
-}
 
 module.exports = {
   timeout: 99999999,
-  reporter: "json",
-  "reporter-option": ["output=" + outPath],
+  reporter: "mocha-json-output-reporter",
+  reporterOptions: {
+    output: path.resolve(__dirname, "../out/ui-test-results.json"),
+  },
 };
