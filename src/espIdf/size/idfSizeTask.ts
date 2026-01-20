8912713b77005b5cc5773c8f32baa610ad87aaea
Brian A. Ignacio <brian.ignacio@espressif.com>
2026-01-20 16:53:59 +0800

    fix get project name idf size task (#1741)
    * fix get project name idf size task
    
    * fix size info task
    
    * Run build tasks before size tasks init

--- src/espIdf/size/idfSizeTask.ts
+++ src/espIdf/size/idfSizeTask.ts
@@ -2,13 +2,13 @@
  * Project: ESP-IDF VSCode Extension
  * File Created: Wednesday, 3rd November 2021 4:56:23 pm
  * Copyright 2021 Espressif Systems (Shanghai) CO LTD
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,36 +29,35 @@ import {
 } from "vscode";
 import { NotificationMode, readParameter } from "../../idfConfiguration";
 import { TaskManager } from "../../taskManager";
-import { appendIdfAndToolsToPath, readProjectCMakeLists } from "../../utils";
+import { appendIdfAndToolsToPath } from "../../utils";
 import { getVirtualEnvPythonPath } from "../../pythonManager";
 import { OutputCapturingExecution } from "../../taskManager/customExecution";
+import { getProjectName } from "../../workspaceConfig";
 
 export class IdfSizeTask {
   private currentWorkspace: Uri;
   private idfSizePath: string;
-  private buildDirPath: string;
 
   constructor(workspaceUri: Uri) {
     this.currentWorkspace = workspaceUri;
     const idfPathDir = readParameter("idf.espIdfPath", workspaceUri) as string;
     this.idfSizePath = join(idfPathDir, "tools", "idf_size.py");
-    this.buildDirPath = readParameter("idf.buildPath", workspaceUri) as string;
-  }
-
-  private mapFilePath() {
-    const projectName = readProjectCMakeLists(this.currentWorkspace.fsPath);
-    return join(this.buildDirPath, `${projectName}.map`);
   }
 
   public async getSizeInfo(captureOutput?: boolean) {
-    await ensureDir(this.buildDirPath);
+    const buildDirPath = readParameter(
+      "idf.buildPath",
+      this.currentWorkspace
+    ) as string;
+    await ensureDir(buildDirPath);
     const pythonCommand = await getVirtualEnvPythonPath(this.currentWorkspace);
-    const mapFilePath = this.mapFilePath();
+    const projectName = await getProjectName(buildDirPath);
+    const mapFilePath = join(buildDirPath, `${projectName}.map`);
     const args = [this.idfSizePath, mapFilePath];
 
     const modifiedEnv = await appendIdfAndToolsToPath(this.currentWorkspace);
     const processOptions = {
-      cwd: this.buildDirPath,
+      cwd: buildDirPath,
       env: modifiedEnv,
     };
 
