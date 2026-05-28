e359c384b13b78de72edc77c016d0495751ce20a
Radu <radu.rentea@gmail.com>
2026-05-28 11:42:45 +0300

    fix(translations): Add missing translations

--- src/espIdf/setTarget/index.ts
+++ src/espIdf/setTarget/index.ts
@@ -168,7 +168,7 @@ export async function setIdfTarget(
           connectedBoards.length > 0
             ? [
                 ...connectedBoards,
-                { kind: QuickPickItemKind.Separator, label: "Default Boards" },
+                { kind: QuickPickItemKind.Separator, label: l10n.t("Default Boards") },
                 ...defaultBoards,
               ]
             : defaultBoards;
